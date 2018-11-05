5f45ea60f6aeb81103e0b31a95cc4bf11d101351
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 18:20:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
       WHERE name = ?
     SQL
  
-    DB[:conn].execute(sql, name).map do |row|
+    p DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
