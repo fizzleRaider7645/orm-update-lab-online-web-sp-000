94b4f7fe1eb89cfd1d52f9eaf2dd2a0cfba02690
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 17:39:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
       VALUES (?, ?);
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    p last_insert = DB[:conn].execute("SELECT * FROM students ORDER BY id DESC LIMIT 1;")
+    p last_insert = DB[:conn].execute("SELECT * FROM students ORDER BY id DESC LIMIT 1;").flatten
     
   end
 end
